4ce12056c2962c87abcf37b88e8f2057abb25cec
Fabio Torti <cartman_7802@yahoo.ie>
2021-12-15 19:47:37 +0000

    added base template

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Gimmione',
+        'title': 'Blog Post 1',
+        'content': 'Gimmione the first',
+        'date_posted': 'December 12, 2021'
+    },
+      {
+        'author': 'Gino Fettuccia',
+        'title': 'Blog Post 2',
+        'content': 'I am Gino',
+        'date_posted': 'December 14, 2021'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
--- blog_app/blog_app/settings.py
+++ blog_app/blog_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-jqqrg02lfc*hbjztx(v1s@cg9&x3rnz9g(!#=b!!y__iphz=@c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
