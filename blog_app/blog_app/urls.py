3bc2d043c69919da1e39c2524e189d69527f39d9
Fabio Torti <cartman_7802@yahoo.ie>
2022-01-16 19:03:29 +0000

    added reseize image for picture profile

--- blog_app/blog_app/urls.py
+++ blog_app/blog_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name= 'users/logout.html'), name='logout'),
     path('profile/', user_views.profile, name='profile'),
     path('', include('blog.urls')),
-]
+] 
 
 
 if settings.DEBUG:
